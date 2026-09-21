3c35b47117b6ad9279dfd71b60158a5068572d18
eggers <brandt.eggers@proton.me>
2026-09-21 11:31:50 -0500

    real pengin!!!
    added the penguin sprite and made it flip when you change direction!
    also added friction

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-@export var ACCELERATION := 10.0
-@export var MAX_SPEED := 50.0
-@export var JUMP_VELOCITY := -400.0
+@export var ACCELERATION := 10.
+@export var MAX_SPEED := 50.
+@export var JUMP_VELOCITY := -400.
+@export var FRICTION := 5.
 
 
 func _physics_process(delta: float) -> void:
@@ -20,5 +21,8 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x += clampf(direction * ACCELERATION, -MAX_SPEED, MAX_SPEED)
+		$Sprite.flip_h = direction == -1
+	# there's probably a better way to write this lol
+	velocity.x += clampf(FRICTION, -absf(velocity.x), absf(velocity.x)) * -1. if velocity.x > 0. else 1.
 
 	move_and_slide()
